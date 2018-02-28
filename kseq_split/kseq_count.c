dd6c625dff0df8f7c05384c154c2cdc78c3243dc
afmcc <alanfmcculloch@gmail.com>
2018-02-28 14:14:04 +1300

    added support of uncompresssed input file

--- kseq_split/kseq_count.c
+++ kseq_split/kseq_count.c
@@ -43,7 +43,33 @@ KSEQ_INIT(gzFile, gzread)
 
 
 /*
-* This program counts the number of logical recrods in  a fastq or fasta, and prints the count to stdout
+* This program counts the number of logical recrods in  a fastq or fasta, and prints the count to stdout.
+* The input file may be compressed or uncompressed. 
+* It has an optional "approximate mode" (-a) , which estimates the number of records by 
+* reading and writing a small preview , of n records, and then estimatog N = n * original file size / preview filesize
+* (if the original is compressed then so is the preview)
+*
+* bugs / limitations associated with the -a option : 
+* 
+*    1. will not recognise .zz compression
+*    2. for all compression types other than gzip, the -a option approximation may be poor (because the compressed preview is always gzip )
+*    3. not extensively tested on formats other than gzip, and uncompressed
+*    4. not tested on variant fastq and fasta 
+* 
+* Example performance of -a option : 
+* time ./kseq_count -a  FM_E-1.fastq.gz
+* 94899536
+*
+* real    0m1.232s
+* user    0m1.219s
+* sys     0m0.012s
+*
+* time ./kseq_count  FM_E-1.fastq.gz
+* 93155200
+*
+* real    6m2.755s
+* user    5m58.003s
+* sys     0m4.278s
 */
 
 
@@ -55,14 +81,14 @@ typedef struct kseqcount_opts {
 
 typedef struct kseqcount_tempfile {
 	char* temp_filename;
-	gzFile temp_file;
-        	
+	gzFile ztemp_file;
+	FILE *temp_file;        	
 } t_kseqcount_tempfile;
 
 
 int get_kseqcount_opts(int argc, char **argv, t_kseqcount_opts *kseqcount_opts)
 {
-	char* usage="Usage: %s [ -h ] filename \n";
+	char* usage="Usage: %s [ -h ] [-a] filename \n";
 	int c;
 
 	kseqcount_opts->approximate = 0; 
@@ -94,7 +120,7 @@ int get_kseqcount_opts(int argc, char **argv, t_kseqcount_opts *kseqcount_opts)
   	return 0;
 }
 
-void kseq_count_write( kseq_t *seq, gzFile fp) {
+void kseq_count_zwrite( kseq_t *seq, gzFile fp) {
 	if ( ! seq->qual.l ) {
 		// assume fasta
 		if ( seq->comment.l ) {
@@ -119,13 +145,40 @@ void kseq_count_write( kseq_t *seq, gzFile fp) {
 	return;
 }
 
+void kseq_count_write( kseq_t *seq, FILE *fp) {
+	if ( ! seq->qual.l ) {
+		// assume fasta
+		if ( seq->comment.l ) {
+			fprintf(fp, ">%s %s\n", seq->name.s, seq->comment.s);
+		}
+		else {
+			fprintf(fp, ">%s\n", seq->name.s);
+		}
+		fprintf(fp, "%s\n", seq->seq.s);
+	}
+	else {
+		// assume fastq		
+        	if ( seq->comment.l ) {
+			fprintf(fp, "@%s %s\n", seq->name.s, seq->comment.s);
+                }
+		else {
+			fprintf(fp, "@%s\n", seq->name.s);
+		}
+		fprintf(fp, "%s\n+\n%s\n", seq->seq.s, seq->qual.s);
+	}
+
+	return;
+}
+
+
 double get_filesize(char *filename) {
 	struct stat stbuf;
 	stat(filename, &stbuf);
 	return (double) stbuf.st_size;
 }
 
-void get_tempfile(t_kseqcount_tempfile *tempfile) {
+void get_tempfile(t_kseqcount_tempfile *tempfile, int compression_type) {
+	// set up either a compressed or standard temp filestream
 	int fp;
         gzFile zp;
 
@@ -133,8 +186,73 @@ void get_tempfile(t_kseqcount_tempfile *tempfile) {
 	tempfile->temp_filename = (char *) malloc(1+strlen(template));
         tempfile->temp_filename = strcpy(tempfile->temp_filename, template);
    	fp=mkstemp(tempfile->temp_filename);
-	tempfile->temp_file=gzdopen(fp,"w");
+
+	if(compression_type != -1) {
+		tempfile->ztemp_file=gzdopen(fp,"w");
+	}
+	else{
+		tempfile->temp_file=fdopen(fp,"w");
+	}
 	return;
+}
+
+int get_compression_type(char *filename) {
+	// ref https://stackoverflow.com/questions/19120676/how-to-detect-type-of-compression-used-on-the-file-if-no-file-extension-is-spe
+	//const char 
+	//Zip (.zip) format description, starts with 0x50, 0x4b, 0x03, 0x04 (unless empty — then the last two are 0x05, 0x06 or 0x06, 0x06)
+	//Gzip (.gz) format description, starts with 0x1f, 0x8b, 0x08
+	//xz (.xz) format description, starts with 0xfd, 0x37, 0x7a, 0x58, 0x5a, 0x00
+
+	//zlib (.zz) format description, starts with two bytes (in bits) 0aaa1000 bbbccccc, where ccccc is chosen so that the 
+        // first byte viewed as a int16 times 256 plus the second byte viewed as a int16 is a multiple of 31. 
+	// e.g: 01111000(bits) = 120(int16), 10011100(bits) = 156(int16), 120 * 256 + 156 = 30876 which is a multiple of 31
+	//compress (.Z) starts with 0x1f, 0x9d
+	//bzip2 (.bz2) starts with 0x42, 0x5a, 0x68
+
+        const char gzip[] = { 0x1f, 0x8b, 0x08 , '\0' };
+	const char zip[] = { 0x50, 0x4b, 0x03, 0x04 ,'\0'};
+	const char zip_empty_a[] = { 0x50, 0x4b, 0x05, 0x06, '\0'};
+	const char zip_empty_b[] = { 0x50, 0x4b, 0x06, 0x06, '\0'};
+	const char compress[] = { 0x1f, 0x9d, '\0'};
+	const char bzip2[] = { 0x42, 0x5a, 0x68, '\0'};
+	const char xz[] = {  0xfd, 0x37, 0x7a, 0x58, 0x5a, 0x00, '\0' };
+
+
+	int finger_length = 0;
+
+	int num_fingerprints;
+        const char* fingerprints[] = { gzip , zip, zip_empty_a, zip_empty_b, compress, bzip2, xz};
+        const int finger_sizes[] = { sizeof(gzip), sizeof(zip), sizeof(zip_empty_a) , sizeof(zip_empty_b), \
+                            sizeof(compress), sizeof(bzip2), sizeof(xz) };
+
+	int buf_length;
+	char fingerprint_buffer[] = {'\0', '\0','\0','\0','\0','\0','\0'};
+	
+	FILE *instream;
+	int num_read=0;
+	int i;
+
+        int compression_type = -1;
+
+	buf_length=( sizeof(fingerprint_buffer) / sizeof(fingerprint_buffer[0]) ) - 1 ;
+        num_fingerprints = sizeof(fingerprints)/ sizeof(fingerprints[0]);
+
+  
+	instream = fopen(filename,"rb");
+	num_read = fread(fingerprint_buffer, buf_length, sizeof(fingerprint_buffer[0]), instream);
+	fclose(instream);
+
+
+	for(i=0; i< num_fingerprints; i++) {
+	   if(strncmp( fingerprint_buffer, fingerprints[i], finger_sizes[i]) == 0) { 
+	      compression_type = i;
+	      break;
+	   }
+        }
+
+	return(compression_type); 
+
+
 }
 
 
@@ -145,6 +263,8 @@ int main(int argc, char *argv[])
 	int l;
         int record_count=0;
         int kseqcount_opts_result = 0;
+	enum compression_types { gzip , zip, zip_empty_a, zip_empty_b };
+	int compression_type = -1;
 
         const int sample_size = 20000;
 
@@ -155,6 +275,12 @@ int main(int argc, char *argv[])
  	if ( kseqcount_opts_result != 0 ){
 		return kseqcount_opts_result;
  	} 
+
+	// check compression type of input file. Note that we only need this for the approximate 
+	// option, in order to decide whether to write out a compressed or uncompressed 
+	// preview file. (zlib can handle either compressed or uncompressed *input* transparently)
+        compression_type = get_compression_type( kseqcount_opts.input_filename) ;
+	
 
 	// process the file 
 	fp = gzopen(kseqcount_opts.input_filename, "r");
@@ -167,10 +293,17 @@ int main(int argc, char *argv[])
 	} // not approximating
 	else {
 		// read and write sample_size records , then count = big_size / small_size * sample_size
-		get_tempfile(&tempfile);
+		// ( - we need to know whether to write compressed or uncompressed records)
+		get_tempfile(&tempfile, compression_type);
 		seq = kseq_init(fp);
 		while((l = kseq_read(seq)) >= 0) {
-			kseq_count_write( seq, tempfile.temp_file);
+			if(compression_type != -1 ) { 
+				kseq_count_zwrite( seq, tempfile.ztemp_file);
+			}
+			else {
+				kseq_count_write( seq, tempfile.temp_file);
+			}
+			
 			record_count++;
 			if(record_count == sample_size) {
 				break;
@@ -181,8 +314,13 @@ int main(int argc, char *argv[])
 
 		//gzprintf(tempfile.temp_file, "Hello World\n");
 		
-		gzclose(tempfile.temp_file);
-		record_count = (int)( 0.5 + sample_size * get_filesize( kseqcount_opts.input_filename ) / get_filesize(tempfile.temp_filename) );
+		if(compression_type != -1) {
+			gzclose(tempfile.ztemp_file);	
+		}
+		else {
+			fclose(tempfile.temp_file);
+		}
+		record_count = (int)( 0.5 + record_count * get_filesize( kseqcount_opts.input_filename ) / get_filesize(tempfile.temp_filename) );
 		//record_count = 0;
 
 	} // approximating
@@ -190,6 +328,7 @@ int main(int argc, char *argv[])
 	// clean up and return
 	kseq_destroy(seq);
 	gzclose(fp);
+	free(tempfile.temp_filename);
         printf ("%d\n", record_count);
 
 	return 0;
